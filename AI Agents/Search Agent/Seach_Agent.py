daf0225226b0394fbe019de17eb0ea490ab62367
Akindu Himan <akinduhiman2@gmail.com>
2024-12-14 22:42:57 +0530

    Search agent improved

--- AI Agents/Search Agent/Seach_Agent.py
+++ AI Agents/Search Agent/Seach_Agent.py
@@ -26,7 +26,7 @@ def initialize_clients():
     }
     
     gemini_model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.0-flash-exp",
         generation_config=generation_config,
     )  
     
@@ -38,7 +38,7 @@ def generate_response(prompt):
     
     chat_session = gemini_model.start_chat()
     final_prompt = f""" 
-    role: system, content: You are a helpful assistant in Sri Lanka that converts the user prompt into a search query to find products. You only provide the search query. No other responds.
+    role: system, content: You are a helpful assistant that converts the user prompt into a search query to find products. You only provide the search query. No other responds.
     role: user, content: {prompt}"""
     response = chat_session.send_message(final_prompt)
     search_query = response.text
@@ -52,7 +52,7 @@ def generate_response(prompt):
             tavily_context_results.append(result['url'])
 
 
-    custom_domains = ["https://www.amazon.com", "https://www.aliexpress.com"]  # Replace with your desired domains
+    custom_domains = ["https://www.amazon.com"]  
 
     # Perform searches for each domain
     for domain in custom_domains:
@@ -68,11 +68,20 @@ def generate_response(prompt):
     print("\n\n")
 
     result_prompt = f""" 
-    role: system, content: Analyze the following search results and provide the web page links.Only provide the links that shows many product results not single product pages. Only give one link from one web domain(Don't give multiple links from same domain/website).Give the results Line by line numbered.
+    role: system, content: Analyze the following search results and provide the web page links.Only provide the links that shows many product results not single product pages. Only give one link from one web domain(Don't give multiple links from same domain/website).Give the results Line by line.
     role: user, content: {tavily_context_results}"""
-    response = chat_session.send_message(result_prompt)
+    response_1 = chat_session.send_message(result_prompt)
 
-    return response.text
+    with open("search_agent_output_1.txt", "a", encoding="utf-8") as f:
+        f.write(response_1.text)
+
+    result_prompt = f""" 
+    role: system, content: Analyze the following search results and provide the web page links.Only provide the links that shows single product in that web page that is {prompt}.
+    role: user, content: {tavily_context_results}"""
+    response_2 = chat_session.send_message(result_prompt)
+
+    with open("search_agent_output_2.txt", "a", encoding="utf-8") as f:
+        f.write(response_2.text)
 
 
-print(generate_response("Type c usb cable"))
+generate_response("canon f166400 printer ink cartridge")
